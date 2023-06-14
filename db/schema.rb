7f8b50ba3f18b62e651dca5c59c6f6d6b8a8b374
Keven da Silva Gonçalves <kevensilvag@alu.ufc.br>
2023-06-14 01:27:49 -0300

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_14_035847) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_14_042436) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,7 +41,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_14_035847) do
     t.string "last_name"
     t.string "telephone"
     t.string "cpf"
+    t.string "jti", null: false
     t.index ["email"], name: "index_clients_on_email", unique: true
+    t.index ["jti"], name: "index_clients_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_clients_on_reset_password_token", unique: true
   end
 
