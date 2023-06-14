c800c6280199475d443044c977286f5912832a73
Keven da Silva Gonçalves <kevensilvag@alu.ufc.br>
2023-06-14 02:09:20 -0300

    update: client model

--- app/models/client.rb
+++ app/models/client.rb
@@ -5,4 +5,6 @@ class Client < ApplicationRecord
 
   devise :database_authenticatable, :registerable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+
+  has_one :vacancy_reservation
 end
