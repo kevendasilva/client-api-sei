d114e0e5b4ffae805fd527c9784c67e5674e1603
Keven da Silva Gonçalves <dasilvagkeven@gmail.com>
2023-06-26 18:28:15 -0300

    update: devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :clients, path: '', path_names: {
-    sign_in: 'login',
-    sign_out: 'logout',
-    registration: 'sign_up'
-  },
-  controllers: {
-    sessions: 'clients/sessions',
-    registrations: 'clients/registrations'
-  }
+  devise_for :clients, skip: [:sessions, :registrations, :passwords]
+  devise_scope :client do
+    # Sessions
+    post   '/login',  to: 'clients/sessions#create',  as: :client_session
+    delete '/logout', to: 'clients/sessions#destroy', as: :destroy_client_session
+    # Registrations
+    put    '/sign_up',  to: 'clients/registrations#update', as: nil
+    delete '/sign_up',  to: 'clients/registrations#destroy', as: nil
+    post   '/sign_up',  to: 'clients/registrations#create', as: nil
+    patch  '/sign_up',  to: 'clients/registrations#update', as: :client_registration
+  end
 
-  get '/current_client', to: 'current_client#index'
+  authenticate do
+    get '/current_client', to: 'current_client#index'
+  end
+
   resources :payment_methods
   get '/search/movements', to: 'search#movements'
   get '/search/parkings', to: 'search#parkings'
